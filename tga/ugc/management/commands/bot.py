c377512e4581e08a80c3d1214c2300c87ea8df67
Alex <soyvitaly@gmail.com>
2021-11-17 09:54:10 +0100

    Генерирую кнопки из БД

--- tga/ugc/management/commands/bot.py
+++ tga/ugc/management/commands/bot.py
@@ -19,16 +19,9 @@ from telegram.ext import (
 from telegram.utils.request import Request
 
 
-# from ugc.models import (
-#     Customers,
-#     OrderStatuses,
-#     Levels,
-#     Forms,
-#     Topping,
-#     Berries,
-#     Decors,
-#     Orders,
-# )
+from ugc.models import (
+    Warehouses
+)
 
 env = Env()
 env.read_env()
@@ -45,9 +38,8 @@ logging.basicConfig(
     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-STORAGE, CHOICE = range(2)
+WAREHOISES, CHOICE = range(2)
 
-storage_buttons = ['Крыло', 'Пирог', 'Комендант', 'Звезда']
 choice_buttons = ['Сезонные вещи', 'Другое']
 
 
@@ -71,28 +63,33 @@ def start(update, context):
 Я помогу вам арендовать личную ячейку для хранения вещей. 
 Давайте посмотрим адреса складов, чтобы выбрать ближайший!
 '''
-    storage_markup = keyboard_maker(storage_buttons, 3)
+
     photo_id = 'AgACAgQAAxkBAAIBMWGUIFP-VyC78fu_jw_Di7gPHcgUAALitzEbG4OgUE4AAaQx910LxQEAAwIAA20AAyIE'
     caption = 'Аренда ячеек в <b>Self Storage</b>'
     bot.send_photo(chat_id=update.message.chat_id,
                    photo=photo_id, caption=caption, parse_mode='HTML')
     update.message.reply_text(text)
     time.sleep(1)
-    second_text = '''
+    warehouses = Warehouses.objects.all()
+    warehouse_buttons = []
+    for warehouse in warehouses:
+        warehouse_buttons.append(warehouse.name)
+    warehouse_markup = keyboard_maker(warehouse_buttons, 2)
+    second_text = f'''
 В Москве 4 склада:
 <b>Крыло</b> - ул. Крыленко, 3Б ( м. Улица Дыбенко )
 <b>Пирог</b> - Пироговская наб., 15 ( м. Площадь Ленина )
 <b>Комендант</b> - пр.Сизова, 2А ( м. Комендантский проспект )
-<b>Звезда</b> - Московское шоссе, 25 ( м. Звездная )   
+<b>Звезда</b> - Московское шоссе, 25 ( м. Звездная )
 '''
-    update.message.reply_text(second_text, reply_markup=storage_markup,
+    update.message.reply_text(second_text, reply_markup=warehouse_markup,
                               parse_mode='HTML')
-    return STORAGE
+    return WAREHOISES
 
 
-def storage(update, context):
+def warehouses(update, context):
     user_message = update.message.text
-    context.user_data['storage'] = user_message
+    context.user_data['warehouse'] = user_message
     choice_markup = keyboard_maker(choice_buttons, 2)
     update.message.reply_text('Что хотите хранить?', reply_markup=choice_markup)
     return CHOICE
@@ -122,7 +119,7 @@ def log_errors(f):
     return inner
 
 
-@log_errors
+#@log_errors
 def end(update, context):
     bot = context.bot
     bot.send_message(
@@ -144,8 +141,8 @@ class Command(BaseCommand):
 
             states={
 
-                STORAGE: [CommandHandler('start', start),
-                       MessageHandler(Filters.text, storage)],
+                WAREHOISES: [CommandHandler('start', start),
+                       MessageHandler(Filters.text, warehouses)],
 
                 CHOICE: [CommandHandler('start', start),
                         MessageHandler(Filters.text, choice)],
